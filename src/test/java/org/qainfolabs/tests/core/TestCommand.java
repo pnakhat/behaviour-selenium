6d4a7822e68f30d61f13cf8cc6b03f4d237ed81e
Pankaj Nakhat <pnakhat@gmail.com>
2012-08-17 07:33:06 +0100

    chnages

--- src/test/java/org/qainfolabs/tests/core/TestCommand.java
+++ src/test/java/org/qainfolabs/tests/core/TestCommand.java
@@ -1,6 +1,5 @@
 package org.qainfolabs.tests.core;
 
-
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.qainfolabs.behaviour.executor.Command;
@@ -12,7 +11,7 @@ public class TestCommand {
 	public void testExtractAction(){
 		String testString = "open,\"http://www.google.co.uk\"";
 		Command cm = new Command(testString);
-		Assert.assertEquals(true, true);
+		Assert.assertEquals(cm.getAction(), "open");
 	}
 
 }
